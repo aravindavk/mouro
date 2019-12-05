1afefc765839e81b3c90a25dcc0049b3ca715b53
Andres Junge <andres.junge@consensys.net>
2019-12-05 10:26:41 +0100

    first try of ws

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import Debug from 'debug'
+const debug = Debug('mouro:server')
+
 //Load Mgrs
 import {DidResolverMgr} from './lib/didResolverMgr';
 import {AuthMgr} from './lib/authMgr';
@@ -18,21 +21,41 @@ const { ApolloServer } = require('apollo-server-express');
 const schema = schemaMgr.getSchema()
 const server = new ApolloServer({
   schema,
-  context: (p:any) => {
-    return ({
+  context: async (p:any) => {
+    //https://www.apollographql.com/docs/apollo-server/features/subscriptions/#context-with-subscriptions
+    if (p.connection) {
+      return p.connection.context;
+    } else {
+      return ({
         headers: p.req.headers
-    });
-},
+      });
+    }
+  },
   introspection: true,
-  graphqlPath:'/graphql'
+  graphqlPath:'/graphql',
+  subscriptions: {
+    onConnect: async (headers:any) => {
+      debug("onConnect: %j",headers);
+      const authData=await authMgr.getAuthData(headers);
+      debug("authData: %j",authData);
+      return {authData};
+    }
+  }
 });
 
-import * as express from "express";
+import express from "express";
 const app = express();
 server.applyMiddleware({ app }); // app is from an existing express app
 
+const http = require('http');
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
+// ⚠️ Pay attention to the fact that we are calling `listen` on the http server variable, and not on `app`.
+httpServer.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-)
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
+})
